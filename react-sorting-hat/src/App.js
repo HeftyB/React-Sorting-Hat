ecbdaeeb29f160a975343aed6f829fa5992d5f2e
Andrew Shields <andrewshields@Scarletts-MBP.attlocal.net>
2020-06-02 00:38:23 -0400

    initial commit

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Link } from "react-router-dom"
+import Home from "./components/Home";
+import Survey from "./components/Survey";
+import Results from "./components/Results";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/">Home</Link>
+      <Link to="/Survey">Survey</Link>
+      <Link to="/Results">Results</Link>
+
+      <Switch>
+        <Route exact path={"/"}>
+          <Home/>
+        </Route>
+        <Route path={"/Survey"}>
+          <Survey></Survey>
+        </Route>
+        <Route path={"/Results"}>
+          <Results/>
+        </Route>
+      </Switch>
+
     </div>
   );
 }
